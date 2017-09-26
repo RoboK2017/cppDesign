0354fd626cc3719365d6a127a260bb2befd1d90a
glc12125 <Liangchuan Gu>
2017-09-26 01:30:16 +0100

    Initial Commit

---
+++ cppDesign/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppDesign
+//
+//  Created by Liangchuan Gu on 26/09/2017.
+//  Copyright © 2017 Liangchuan Gu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
